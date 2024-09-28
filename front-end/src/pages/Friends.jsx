10b482ff7a1d565941e4c578e7397c565fbca3c6
Jaiveer Yadav <dev.jaiveer.yadav@gmail.com>
2024-09-28 12:29:22 +0530

    fixed the nav color effect

---
+++ front-end/src/pages/Friends.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header";
+import Navigation from "../components/Navigation";
+
+function Friends() {
+  return (
+    <div className="home h-screen bg-[#0A090F] w-full">
+      <Header />
+      <div className="main h-full flex flex-col items-center justify-start w-full px-4">
+
+      </div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default Friends;
--- front-end/src/routes/IndexRouer.jsx
+++ front-end/src/routes/IndexRouer.jsx
@@ -1,7 +1,10 @@
 import { Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Friends from "../pages/Friends";
 
 const IndexRouer = [
   <Route key="index" path="/" element={<Home />} />,
+  <Route key="friend" path="/friends" element={<Friends />} />,
+
 ];
 export default IndexRouer;
